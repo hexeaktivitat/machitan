74805490d4f430e76b12aded2b23e2095c58d1db
hexeaktivitat <hexeaktivitat@gmail.com>
2024-03-19 16:38:19 -0400

    default plugins

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct Person;
+
+#[derive(Component)]
+pub struct Name(String);
+
 pub fn hello_world() {
     println!("hello, world");
+}
+
+pub fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Silence Suzuka".into())));
+    commands.spawn((Person, Name("Special Week".into())));
+    commands.spawn((Person, Name("Tokai Teio".into())));
+}
+
+pub fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}", name.0);
+    }
+}
+
+pub fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Special Week" {
+            name.0 = "Nice Nature".into();
+            break;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,9 @@ use bevy::prelude::*;
 use machitan::*;
 
 fn main() {
-    App::new().add_systems(Update, hello_world).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .run();
 }
